bb530987d522b133badfb671b25803e8255b0781
Thang <tp375883@gmail.com>
2021-02-22 18:15:22 +0700

    thang

--- Fontend/Lucky13/src/app/app-routing.module.ts
+++ Fontend/Lucky13/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'signin', loadChildren: () => import('./Thang/signin/signin.module').then(m => m.SigninModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
