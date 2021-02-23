9911480eeadd0e77a17ee7e765b38b5fd88bc239
Huynh Duc <hsu.tranhuynhduc@gmail.com>
2021-02-23 17:24:29 +0700

    none

--- Fontend/Lucky13/src/app/app-routing.module.ts
+++ Fontend/Lucky13/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'signup', loadChildren: () => import('./Trong/signup/signup.module').then(m => m.SignupModule) }];
+const routes: Routes = [{ path: 'signup', loadChildren: () => import('./Trong/signup/signup.module').then(m => m.SignupModule) }, { path: 'ConnectSocket', loadChildren: () => import('./Duc/connect-socket/connect-socket.module').then(m => m.ConnectSocketModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
