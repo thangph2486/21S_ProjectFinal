0b163cfd0cf9d59d7027fadb52e09c532ec43755
Huynh Duc <hsu.tranhuynhduc@gmail.com>
2021-02-24 11:44:58 +0700

    mearge long

--- Fontend/Lucky13/src/app/app-routing.module.ts
+++ Fontend/Lucky13/src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'signup', loadChildren: () => import('./Trong/signup/signup.module').then(m => m.SignupModule) },
   { path: 'xuatbai', loadChildren: () => import('./Trong/xuatbai/xuatbai.module').then(m => m.XuatbaiModule) },
-  { path: 'ConnectSocket', loadChildren: () => import('./Duc/connect-socket/connect-socket.module').then(m => m.ConnectSocketModule) }
+  { path: 'ConnectSocket', loadChildren: () => import('./Duc/connect-socket/connect-socket.module').then(m => m.ConnectSocketModule) },
+  { path: 'aboutus', loadChildren: () => import('./Thang/aboutus/aboutus.module').then(m => m.AboutusModule) },
+  { path: 'sigin', loadChildren: () => import('./Thang/signin/signin.module').then(m => m.SigninModule) },
+
 
 ];
 
