d8fa0130ed8e3d7b5aa79dc604b6e0a5ac71f9c0
unknown <trong.phamtranduc@gmail.com>
2021-02-26 15:12:08 +0700

    danh bai

--- Fontend/Lucky13/src/app/app.module.ts
+++ Fontend/Lucky13/src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import {MatButtonModule} from '@angular/material/button';
 const config: SocketIoConfig = { url: '192.168.31.188:3000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
