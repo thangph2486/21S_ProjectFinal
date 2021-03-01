0b66d355f50da6b8b5f1eb895c535e1797af50c9
Huynh Duc <hsu.tranhuynhduc@gmail.com>
2021-03-01 13:54:17 +0700

    fix viewPlay

--- Fontend/Lucky13/src/app/Long/pages/play/listshowcards/cardview/cardview.component.ts
+++ Fontend/Lucky13/src/app/Long/pages/play/listshowcards/cardview/cardview.component.ts
@@ -8,17 +8,16 @@ import { CardDataService } from 'src/app/services/card-data.service';
   styleUrls: ['./cardview.component.scss']
 })
 export class CardviewComponent implements OnInit {
-  constructor(private cardService: CardDataService) {}
+  constructor(private cardService: CardDataService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   @Input()
   card: Card;
 
-  outCard(card){
+  outCard(card) {
     const element = <HTMLElement>document.getElementById(card);
     element.style.marginBottom.valueOf() == '100px'
-      element.style.marginLeft = '150px';
-    console.log(this.cardService.temp);
+    element.style.marginLeft = '150px';
   }
 }
 
