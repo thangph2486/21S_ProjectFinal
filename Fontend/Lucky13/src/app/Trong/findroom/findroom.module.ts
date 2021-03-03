0cc373ecb5d5ff72f555852bc99139a747453b00
Huynh Duc <hsu.tranhuynhduc@gmail.com>
2021-03-03 16:59:58 +0700

    merge Trong

--- Fontend/Lucky13/src/app/Trong/findroom/findroom.module.ts
+++ Fontend/Lucky13/src/app/Trong/findroom/findroom.module.ts
@@ -4,19 +4,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FindroomRoutingModule } from './findroom-routing.module';
 import { FindroomComponent } from './findroom.component';
 import { NavFindroomComponent } from './components/nav-findroom/nav-findroom.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon'
-import { MatTableModule } from '@angular/material/table';
-import { UserComponent } from './components/user/user.component';
-import { MatButtonModule } from '@angular/material/button';
 import { CreateRoomComponent } from './components/create-room/create-room.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
+import { MatTableModule } from '@angular/material/table';
+import { RoomComponent } from './components/listroom/room/room.component';
+import { UserComponent } from './components/user/user.component';
+import { MatButtonModule } from '@angular/material/button';
 import { ListroomComponent } from './components/listroom/listroom.component';
 
 @NgModule({
-  declarations: [FindroomComponent, NavFindroomComponent, UserComponent, ListroomComponent, CreateRoomComponent],
+  declarations: [FindroomComponent, NavFindroomComponent, RoomComponent, UserComponent, ListroomComponent],
   imports: [
     CommonModule,
     FindroomRoutingModule,
@@ -28,8 +29,7 @@ import { ListroomComponent } from './components/listroom/listroom.component';
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule
+    MatInputModule
   ]
 })
 export class FindroomModule { }
